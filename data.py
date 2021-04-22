988a42c8dcb82dc90f30a8257e0b27f7391bea5c
foxjazz <fox21@foxjazz.net>
2021-04-21 19:52:07 -0600

    first

---
+++ data.py
@@ -0,0 +1,87 @@
+theForge = 10000002
+Jita = 30000142
+JitaMarket = 60003760
+stations = [60000364,60000451,60000463, 60002953, 60002959, 60003055, 60003460, 60003463, 60003466, 60003469, 60003757, 60003760, 60004423]
+astroidCatgories=[450,451,452,453,454,455,456,457,458,459,460,461,462,465,467,468,469,519,903,1884,1911,1920,1921,1922,1923,2006,2022,2024,4029,4030,4031]
+veldspar = {"id": 28432, "trit": 400}
+concentratedveldspar = {"id": 28430, "trit": 420}
+denseveldspar = {"id": 28431, "trit": 440}
+stableveldspar = {"id": 46705, "trit": 460}
+
+scordite = {"id": 28429, "trit": 150, "pye": 90}
+condensedscordite = {"id": 28427, "trit": 158, "pye": 95}
+glossyscordite = {"id": 46687, "trit": 173, "pye": 104}
+massivescordite = {"id": 28428, "trit": 165, "pye": 99}
+
+
+pyroxeres = {"id": 28422, "pye": 90, "mex": 30}
+opulantpyroxeres = {"id": 46702, "pye": 104, "mex": 35}
+solidpyroxeres = {"id": 28425, "pye": 95, "mex": 32}
+viscouspyroxeres = {"id": 28426, "pye": 99, "mex": 33}
+
+plagioclase= {"id": 28422, "trit": 175, "mex": 70}
+azureplagioclase= {"id": 28421, "trit": 184, "mex": 74}
+richplagioclase= {"id": 28423, "trit": 193, "mex": 77}
+sparklingplagioclase= {"id": 46701, "trit": 201, "mex": 81}
+
+omber = {"id": 28416, "pye": 90, "iso": 75}
+goldenomber = {"id": 28415, "pye": 99, "iso": 83}
+platinoidomber = {"id": 46700, "pye": 104, "iso": 86}
+silveryomber = {"id": 28417, "pye": 95, "iso": 79}
+
+kernite={"id": 28410, "mex": 60, "iso": 120}
+fierykernite={"id": 28409, "mex": 66, "iso": 132}
+luminouskernite={"id": 28411, "mex": 63, "iso": 126}
+resplendantkernite={"id": 46699, "mex": 69, "iso": 138}
+
+
+jaspet={"id": 28406, "mex": 150, "nocx": 50}
+immaculatejaspet={"id": 46698, "mex": 173, "nocx": 58}
+pristinejaspet={"id": 28407, "mex": 165, "nocx": 55}
+purejaspet={"id": 28408, "mex": 158, "nocx": 53}
+
+hemorphite={"id": 28403, "iso": 240, "nocx": 90 }
+radianthemorphite={"id": 28404, "iso": 284, "nocx": 99 }
+scintillatinghemorphite={"id": 46697, "iso": 276, "nocx": 104 }
+vividhemorphite={"id": 28405, "iso": 252, "nocx": 99 }
+
+hedbergite= {"id": 28401, "pye": 450, "nocx": 120}
+glazedhedbergite= {"id": 28400, "pye": 495, "nocx": 132}
+lustroushedbergite= {"id": 46696, "pye": 518, "nocx": 138}
+vitrichedbergite= {"id": 28402, "pye": 473, "nocx": 126}
+
+darkochre= {"id": 28394, "mex": 1360, "iso": 1200, "nocx": 320}
+jetochre= {"id": 46694, "mex": 1584, "iso": 1380, "nocx": 368}
+obsidianochre= {"id": 28395, "mex": 1496, "iso": 1320, "nocx": 352}
+onyxochre= {"id": 28396, "mex": 1428, "iso": 1260, "nocx": 336}
+
+gneiss={"id": 0, "pye": 2000, "mex": 1500, "iso": 800}
+brilliantgneiss={"id": 0, "pye": 2300, "mex": 1725, "iso": 920}
+irradescentgneiss={"id": 0, "pye": 2100, "mex": 1575, "iso": 840}
+prismaticgneiss={"id": 0, "pye": 2, "mex": 1650, "iso": 880}
+
+crokite={"id": 28391, "pye": 800, "mex": 2000, "nocx": 800}
+crystallinecrokite={"id": 28392, "pye": 880, "mex": 2200, "nocx": 880}
+pellucidcrokite={"id": 46693, "pye": 920, "mex": 2300, "nocx": 920}
+sharpcrokite={"id": 28393, "pye": 840, "mex": 2100, "nocx": 840}
+
+bistot={"id": 28388, "pye": 3200, "mex": 1200, "zyd": 160}
+triclinicbistot={"id": 28390, "pye": 3360, "mex": 1260, "zyd": 168}
+cubicbistot={"id": 46692, "pye": 3680, "mex": 1380, "zyd": 184}
+monoclinicbistot={"id": 28389, "pye": 3520, "mex": 1320, "zyd": 176}
+
+arkonor={"id": 28367, "pye": 3200, "mex": 1200, "mega": 120}
+crimsonarkonor={"id": 28385, "pye": 3360, "mex": 1260, "mega": 126}
+flawlessarkonor={"id": 46691, "pye": 3680, "mex": 1380, "mega": 138}
+primearkonor={"id": 28387, "pye": 3520, "mex": 1320, "mega": 132}
+
+spodumain={"id": 28420, "trit": 48000,"iso":1000, "nocx": 160, "zyd": 80, "mega": 40}
+gleamingSpodumain={"id": 28419, "trit": 52800, "iso": 1100, "nocx": 176, "zyd": 88, "mega": 44}
+dazzlingSpodumain={"id": 46704, "trit": 55200, "iso": 1150, "nocx": 184, "zyd": 92, "mega": 48}
+brightSpodumain={"id": 28418, "trit": 50400, "iso": 1050, "nocx": 168, "zyd": 84, "mega": 42}
+
+
+
+
+
+
---
+++ market.py
@@ -0,0 +1,36 @@
+# https://esi.evetech.net/ui/?version=latest#/
+
+import requests
+import data
+
+marketUri = "https://esi.evetech.net/latest/markets/10000002/orders/?datasource=tranquility&order_type=sell&page=1&type_id="
+compressedOre={data.arkonor, data.primearkonor, data.crimsonarkonor}
+
+groupUri = "https://esi.evetech.net/latest/universe/groups/?datasource=tranquility&page=1"
+# groups = requests.get(url=groupUri).json()
+astroidItem = {}
+itemUri = "https://esi.evetech.net/latest/universe/types/8/?datasource=tranquility&language=en"
+groupIdUri = "https://esi.evetech.net/latest/universe/groups/25/?datasource=tranquility&language=en"
+for g in data.astroidCatgories:
+    gurl = "https://esi.evetech.net/latest/universe/groups/"+str(g)+"/?datasource=tranquility&language=en"
+    response = requests.get(url=gurl)
+    group = response.json()
+    types = group['types']
+    for t in types:
+        typeUrl = "https://esi.evetech.net/latest/universe/types/" + str(t) + "/?datasource=tranquility&language=en"
+        data = requests.get(url=typeUrl).json()
+        print(data['name'], data['type_id'])
+        astroidItem["data['name']"] = data['type_id']
+
+stations = []
+
+for d in stations:
+    print("stationId:" , d)
+    url = "https://esi.evetech.net/latest/universe/stations/" + str(d) + "/?datasource=tranquility"
+    response = requests.get(url=url)
+    data = response.json()
+    print(data)
+
+
+
+
